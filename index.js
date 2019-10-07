48c2fd782e94ef37b57b26e0df277011ac0874af
Wassim-Alouini <52957875+Wassim-Alouini@users.noreply.github.com>
2019-10-07 22:58:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const pets = [
   { name: `Max`, type: `dog`, bornOn: 2018 },
-  { name: `Angel`, type: `cat`, catbornOn: 2015 },
+  { name: `Angel`, type: `cat`, bornOn: 2015 },
   { name: `Jasper`, type: `dog`, bornOn: 2016 }
 ];
 
@@ -13,15 +13,15 @@ pets.forEach((item) => {
   item.age = getAge(item)
   petsWithAge.push(item)
 })
-document.querySelector(#array1).outerHTML = (petsWithAge);
+document.querySelector("#array1").outerHTML = JSON.stringify(petsWithAge);
 
 const dogs = petsWithAge.filter((item) => {
   return item.type == `dog`
 })
-document.querySelector(#array2).outerHTML = (dogs)
+document.querySelector("#array2").outerHTML = JSON.stringify(dogs);
 
 const jasper = petsWithAge.filter((item) => {
   return item.name == `Jasper`
 })
 
-document.querySelector(#jasper).outerHTML = (`Jasper is ${jasper[0].age} years old`);
+document.querySelector("#jasper").outerHTML = (`Jasper is ${jasper[0].age} years old`);
